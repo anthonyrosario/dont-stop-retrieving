6d463d489f79d83346f7aec673f28807991bc073
Anthony Rosario <rosariojanthony@gmail.com>
2016-05-27 14:47:48 -0700

    create map

--- scripts/models/maps.js
+++ scripts/models/maps.js
@@ -38,10 +38,4 @@ var mapOptions = {
 
 var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-google.maps.event.addDomListener(window, 'resize', function() {
-  var center = map.getCenter();
-  google.maps.event.trigger(map, 'resize');
-  map.setCenter(center);
-});
-
 })();
