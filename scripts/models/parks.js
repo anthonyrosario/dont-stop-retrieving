6749815479c2d6ce8ca2485f4dbcdef9458ca058
Anthony Rosario <rosariojanthony@gmail.com>
2016-05-30 12:20:37 -0700

    fixed ajax call for parks data

--- scripts/models/parks.js
+++ scripts/models/parks.js
@@ -4,17 +4,14 @@
   parksData.all = [];
 
   parksData.getDogParks = function() {
-    $.ajax({
-    url: 'https://data.seattle.gov/resource/3c4b-gdxv.json',
-    type: 'GET',
-    headers: { access_token: process.env.PARKS_TOKEN },
-    success: function(data, message, xhr) {
+
+  $.get('https://data.seattle.gov/resource/3c4b-gdxv.json')
+    .done(function(data, message, xhr) {
       parksData.all = data.filter(function(obj) {
         return obj.city_feature === 'Off Leash Areas'
-      }
-    }
-  }
+      });
+    })
+}
 
-  parksData.getDogParks();
   module.parksData = parksData;
 })(window)
