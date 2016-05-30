6cb5b564d8d97f7526251dfece02dc257f380bd0
Tracey <radcliffe.tracey@gmail.com>
2016-05-30 11:09:43 -0700

    trying to get autocomplete working for classes

--- scripts/views/mapsView.js
+++ scripts/views/mapsView.js
@@ -1,13 +1,25 @@
-function initMap() {
+(function(module){
+
   var $mapDiv = $('.map');
-  var map = new google.maps.Map($mapDiv[0], {
-    center: {lat: 47.6050, lng: -122.3344},
+
+  var myOptions = {
     zoom: 12,
+    center: new google.maps.LatLng(47.618217, -122.351832),
+    mapTypeId: google.maps.MapTypeId.STREET,
     zoomControl: true,
     zoomControlOptions: {
       position: google.maps.ControlPosition.RIGHT_CENTER
     }
-  });
-}
+  };
 
-initMap();
+  var map1 = new google.maps.Map($mapDiv[0], myOptions);
+  var map2 = new google.maps.Map($mapDiv[1], myOptions);
+
+  function initialize() {
+    var input = document.getElementsByClassName('search-box');
+    var autocomplete = new google.maps.places.Autocomplete(input);
+  }
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+
+})();
