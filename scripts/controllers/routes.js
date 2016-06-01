78b0d86c257c585bebd26697e8753b64474aba1e
keiran beaton <keiran beaton>
2016-06-01 11:46:39 -0700

    testing to see if fixed location bug

--- scripts/controllers/routes.js
+++ scripts/controllers/routes.js
@@ -1,5 +1,6 @@
 page('/', homeController.index);
-page('/all-parks', parkDataController.index);
+page('/all-parks', clearArray, parkDataController.index);
+page('/closest-parks', clearArray, nearestParksController.index);
 page('/about', aboutController.index);
 page('/park/:id', singleParkController.loadSinglePark, singleParkController.index);
 page();
