0d9ad35293dd7d0a3b17081deef063c82d6501d9
keiran beaton <keiran beaton>
2016-06-02 11:58:04 -0700

    fixed submit button issues

--- scripts/views/homeView.js
+++ scripts/views/homeView.js
@@ -5,10 +5,5 @@
   homeView.initHomePage = function() {
     $('#home').fadeIn('fast').siblings().hide();
   };
-  $('#submit').on('click', function() {
-    scroll(0,0);
-    $('#results').fadeIn('fast').siblings().hide();
-    $('#home-page-search').val('');
-  });
   module.homeView = homeView;
 })(window);
--- scripts/views/nearestParksView.js
+++ scripts/views/nearestParksView.js
@@ -3,8 +3,13 @@
 
   nearestParksView.initResults = function() {
     var nearestParks = [];
+    scroll(0,0);
+    $('#home-page-search').val('');
     $('section').hide();
     $('#results').fadeIn('fast');
+    var center = map1.getCenter();
+    google.maps.event.trigger($('#map-one')[0], 'resize');
+    map1.setCenter(center);
     $('.search-results').empty();
     nearestParks = MapLocation.findDistance(Park.all, userLat, userLng);
     nearestParks.forEach(function(park){
